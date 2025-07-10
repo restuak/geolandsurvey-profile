18291f8e639c489b39430228a3be32c3e3d1af87
restuak <restuaka.works@gmail.com>
2025-07-11 00:40:13 +0700

    feat: hero edit

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,24 +1,24 @@
 
-// export default function Hero () {
-//     return (
-//       <div
-//         className="hero h-screen"
-//         style={{
-//           backgroundImage: "url(../2.jpg)",
-//         }}
-//       >
-//         <div className="hero-overlay"></div>
-//         <div className="hero-content text-neutral-content text-center">
-//           <div className="max-w-md">
-//             <h1 className="mb-5 text-5xl font-bold">Geoland Survey</h1>
-//             <p className="mb-5">
-//               Let's talk about geospatial and solve your problem together.
-//             </p>
-//             <button className="btn btn-primary bg-[#992b39] border-[#0C566D] ">
-//               <a href="/#service">OUR SERVICE</a>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     );
-// }
+export default function Hero () {
+    return (
+      <div
+        className="hero h-screen"
+        style={{
+          backgroundImage: "url(../2.jpg)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Geoland Survey</h1>
+            <p className="mb-5">
+              Let's talk about geospatial and solve your problem together.
+            </p>
+            <button className="btn btn-primary bg-[#992b39] border-[#0C566D] ">
+              <a href="/#service">OUR SERVICE</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+}
